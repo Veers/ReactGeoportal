e6e32402eac999f13ec8db020708ebb88557bb89
Veers <a.konviser@gmail.com>
2018-01-12 01:04:02 +0300

    change form code

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,85 +5,107 @@ import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton'
 import Checkbox from 'material-ui/Checkbox'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
+import validate from './validate'
 
-const validate = values => {
-  const errors = {}
-  const requiredFields = [ 'firstName', 'lastName', 'email', 'favoriteColor', 'notes' ]
-  requiredFields.forEach(field => {
-    if (!values[ field ]) {
-      errors[ field ] = 'Required'
-    }
-  })
-  if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address'
-  }
-  return errors
-}
-
-const renderTextField = props => (
-  <TextField hintText={props.label}
-    floatingLabelText={props.label}
-    errorText={props.touched && props.error}
-    {...props}
+const renderTextField = (
+  { input, label, meta: { touched, error }, ...custom },
+) => (
+  <TextField
+    hintText={label}
+    floatingLabelText={label}
+    errorText={touched && error}
+    {...input}
+    {...custom}
   />
 )
 
-const renderCheckbox = props => (
-  <Checkbox label={props.label}
-    checked={props.value ? true : false}
-    onCheck={props.onChange}/>
+const renderCheckbox = ({ input, label }) => (
+  <Checkbox
+    label={label}
+    checked={input.value ? true : false}
+    onCheck={input.onChange}
+  />
 )
 
-const renderSelectField = props => (
+const renderRadioGroup = ({ input, ...rest }) => (
+  <RadioButtonGroup
+    {...input}
+    {...rest}
+    valueSelected={input.value}
+    onChange={(event, value) => input.onChange(value)}
+  />
+)
+
+const renderSelectField = (
+  { input, label, meta: { touched, error }, children, ...custom },
+) => (
   <SelectField
-    floatingLabelText={props.label}
-    errorText={props.touched && props.error}
-    {...props}
-    onChange={(event, index, value) => props.onChange(value)}>
-  </SelectField>
+    floatingLabelText={label}
+    errorText={touched && error}
+    {...input}
+    onChange={(event, index, value) => input.onChange(value)}
+    children={children}
+    {...custom}
+  />
 )
 
 const SearchForm = props => {
-  const { handleSubmit, pristine, reset, submitting } = props
+  const { handleSubmit, pristine, reset, submitting } = props;
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <Field name="firstName" component={renderTextField} label="First Name"/>
+        <Field
+          name="firstName"
+          component={renderTextField}
+          label="First Name"
+        />
       </div>
       <div>
-        <Field name="lastName" component={renderTextField} label="Last Name"/>
+        <Field name="lastName" component={renderTextField} label="Last Name" />
       </div>
       <div>
-        <Field name="email" component={renderTextField} label="Email"/>
+        <Field name="email" component={renderTextField} label="Email" />
       </div>
       <div>
-        <Field name="sex" component={RadioButtonGroup}>
-          <RadioButton value="male" label="male"/>
-          <RadioButton value="female" label="female"/>
+        <Field name="sex" component={renderRadioGroup}>
+          <RadioButton value="male" label="male" />
+          <RadioButton value="female" label="female" />
         </Field>
       </div>
       <div>
-        <Field name="favoriteColor" component={renderSelectField} label="Favorite Color">
-          <MenuItem value={'ff0000'} primaryText="Red"/>
-          <MenuItem value={'00ff00'} primaryText="Green"/>
-          <MenuItem value={'0000ff'} primaryText="Blue"/>
+        <Field
+          name="favoriteColor"
+          component={renderSelectField}
+          label="Favorite Color"
+        >
+          <MenuItem value="ff0000" primaryText="Red" />
+          <MenuItem value="00ff00" primaryText="Green" />
+          <MenuItem value="0000ff" primaryText="Blue" />
         </Field>
       </div>
       <div>
-        <Field name="employed" component={renderCheckbox} label="Employed" value="true"/>
+        <Field name="employed" component={renderCheckbox} label="Employed" />
       </div>
       <div>
-        <Field name="notes" component={renderTextField} label="Notes" multiLine={true} rows={2}/>
+        <Field
+          name="notes"
+          component={renderTextField}
+          label="Notes"
+          multiLine={true}
+          rows={2}
+        />
       </div>
       <div>
-        <button type="submit" >Submit</button>
-        <button type="button" onClick={reset}>Clear Values</button>
+        <button type="submit" disabled={pristine || submitting}>Submit</button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
       </div>
     </form>
   )
 }
 
 export default reduxForm({
-  form: 'SearchForm',  // a unique identifier for this form
-  validate
+  form: 'SearchForm', // a unique identifier for this form
+  validate,
 })(SearchForm)
