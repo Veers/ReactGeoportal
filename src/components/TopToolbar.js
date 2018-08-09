ed6284e3b9c0395050584f418c551f691a3f2ea9
Veers <a.konviser@gmail.com>
2018-08-10 00:17:07 +0300

    rethinking tools

--- src/components/TopToolbar.js
+++ src/components/TopToolbar.js
@@ -4,24 +4,38 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Switch from '@material-ui/core/Switch';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
-import NavigationClose from 'material-ui/svg-icons/navigation/close'
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+  }
+});
 
 class TopToolbar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      logged: true
+      logged: true,
+      projection: '123',
+      isMenuOpen: false
     }
     this.handleEvent = this.handleEvent.bind(this)
   }
@@ -31,45 +45,27 @@ class TopToolbar extends Component {
     console.log(event)
   }
 
+  clickMenu = event => {
+    this.setState((prevState) => ({
+      isMenuOpen: !prevState.isMenuOpen
+    }));
+  }
+
   render() {
-    const classes = {
-      root: {
-        flexGrow: 1,
-      },
-      flex: {
-        flexGrow: 1,
-      },
-      menuButton: {
-        marginLeft: -12,
-        marginRight: 20,
-      },
-    };
+    const { classes } = this.props;
 
     return (
       <div>
         <MuiThemeProvider>
           <AppBar position="static">
             <Toolbar>
-              <IconButton color="inherit" aria-label="Menu">
+              <IconButton color="inherit" aria-label="Menu" onClick={this.clickMenu}>
                 <MenuIcon />
               </IconButton>
               <Typography variant="title" color="inherit">
                 News
               </Typography>
               <Switch id="mapSwitcher" defaultChecked value="checkedF" color="default" /> c123
-              <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="age-helper">Age</InputLabel>
-                <Select
-                  value={this.state.age}
-                  onChange={this.handleChange}
-                  input={<Input name="age" id="age-helper" />}
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-                <FormHelperText>Projection select</FormHelperText>
-              </FormControl>
             </Toolbar>
           </AppBar>
         </MuiThemeProvider>
@@ -78,4 +74,6 @@ class TopToolbar extends Component {
   }
 }
 
-export default TopToolbar;
+export default withStyles(styles)(TopToolbar);
+
+// export default TopToolbar;
